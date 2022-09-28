8c37f934efb87b30970cfbb2b4eb67383dfebfcf
alby11 <17138674+alby11@users.noreply.github.com>
2022-09-28 07:41:02 +0200

    Wed Sep 28 07:41:02 AM CEST 2022

--- .dotfiles_sync.sh
+++ .dotfiles_sync.sh
@@ -1,7 +1,11 @@
-if command -v dotfiles
-then
-  dotfiles add .bash* .zsh* bootstrap.sh .setup_dotfiles.sh .oh-my-zsh* .tmux.conf .config/bat .config/btop .config/ranger catppuccin_tty zsh-syntax-highlighting .dotfiles_sync.sh -v
-fi \
-  && dotfiles commit -m "$(date)" -v \
-  && dotfiles push -v && \
-  source .zshrc
+#!/bin/zsh
+curDir=$(pwd)
+cd $HOME
+function dotfiles {
+   git --git-dir=$HOME/.dotfiles_git/ --work-tree=$HOME $@
+}
+dotfiles add .bash* .zsh* bootstrap.sh .setup_dotfiles.sh .oh-my-zsh* .tmux.conf .config/bat .config/btop .config/ranger catppuccin_tty zsh-syntax-highlighting .dotfiles_sync.sh -v
+dotfiles commit -m "$(date)" -v
+dotfiles push -v
+source .zshrc
+cd $curDir
