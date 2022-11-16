4fac92e96bc1565c2e7827ed693eb40f147d4877
alby11 <17138674+alby11@users.noreply.github.com>
2022-11-16 10:09:31 +0000

    tested on physical and edited

--- .config/zsh/zsh_plugins.zsh
+++ .config/zsh/zsh_plugins.zsh
@@ -1,72 +1,72 @@
 if ! (( $+functions[zsh-defer] )); then
-  fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer )
-  source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer/zsh-defer.plugin.zsh
+  fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer )
+  source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer/zsh-defer.plugin.zsh
 fi
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-export PATH="/home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-bench:$PATH"
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/bzr.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/clipboard.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/cli.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/compfix.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/completion.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/correction.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/diagnostics.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/directories.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/functions.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/git.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/grep.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/history.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/key-bindings.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/misc.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/nvm.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/prompt_info_functions.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/spectrum.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/termsupport.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/theme-and-appearance.zsh
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/vcs_info.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/editor )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/editor/editor.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/history )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/history/history.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/prompt )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/prompt/prompt.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/utility )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/utility/utility.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/completion )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/completion/completion.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zfunctions )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zfunctions/zfunctions.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
-zsh-defer source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wulfgarpro-SLASH-history-sync )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wulfgarpro-SLASH-history-sync/history-sync.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-fzf-zsh-plugin )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-fzf-zsh-plugin/fzf-zsh-plugin.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-srijanshetty-SLASH-zsh-pip-completion )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-srijanshetty-SLASH-zsh-pip-completion/zsh-pip-completion.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-3v1n0-SLASH-zsh-bash-completions-fallback )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-3v1n0-SLASH-zsh-bash-completions-fallback/zsh-bash-completions-fallback.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MenkeTechnologies-SLASH-zsh-cargo-completion )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MenkeTechnologies-SLASH-zsh-cargo-completion/zsh-cargo-completion.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-docker-zsh-completion )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-docker-zsh-completion/docker-zsh-completion.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion/zsh-better-npm-completion.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sunlei-SLASH-zsh-ssh )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sunlei-SLASH-zsh-ssh/zsh-ssh.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-vasyharan-SLASH-zsh-brew-services )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-vasyharan-SLASH-zsh-brew-services/zsh-brew-services.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-viasite-ansible-SLASH-zsh-ansible-server )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-viasite-ansible-SLASH-zsh-ansible-server/ansible-server.plugin.zsh
-fpath+=( /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use )
-source /home/alby11/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use/you-should-use.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+export PATH="/home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-bench:$PATH"
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/bzr.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/cli.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/clipboard.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/compfix.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/completion.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/correction.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/diagnostics.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/directories.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/functions.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/git.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/grep.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/history.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/key-bindings.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/misc.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/nvm.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/prompt_info_functions.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/spectrum.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/termsupport.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/theme-and-appearance.zsh
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/vcs_info.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/editor )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/editor/editor.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/history )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/history/history.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/prompt )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/prompt/prompt.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/utility )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/utility/utility.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/completion )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-belak-SLASH-zsh-utils/completion/completion.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zfunctions )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zfunctions/zfunctions.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
+zsh-defer source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wulfgarpro-SLASH-history-sync )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wulfgarpro-SLASH-history-sync/history-sync.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-fzf-zsh-plugin )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-fzf-zsh-plugin/fzf-zsh-plugin.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-srijanshetty-SLASH-zsh-pip-completion )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-srijanshetty-SLASH-zsh-pip-completion/zsh-pip-completion.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-3v1n0-SLASH-zsh-bash-completions-fallback )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-3v1n0-SLASH-zsh-bash-completions-fallback/zsh-bash-completions-fallback.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MenkeTechnologies-SLASH-zsh-cargo-completion )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MenkeTechnologies-SLASH-zsh-cargo-completion/zsh-cargo-completion.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-docker-zsh-completion )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-docker-zsh-completion/docker-zsh-completion.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion/zsh-better-npm-completion.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sunlei-SLASH-zsh-ssh )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sunlei-SLASH-zsh-ssh/zsh-ssh.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-vasyharan-SLASH-zsh-brew-services )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-vasyharan-SLASH-zsh-brew-services/zsh-brew-services.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-viasite-ansible-SLASH-zsh-ansible-server )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-viasite-ansible-SLASH-zsh-ansible-server/ansible-server.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use )
+source /home/tallonea/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use/you-should-use.plugin.zsh
