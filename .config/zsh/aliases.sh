3f6aec38e379c455f1ffa5623cccadde2310f486
alby11 <alby11@duck.com>
2023-08-22 13:51:41 +0200

    20230822_13:51:40

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -71,158 +71,160 @@ fi
 
 if command -v systemctl &>/dev/null; then
     # Basic systemctl commands
-    alias systemctl="sudo systemctl "
-    alias ctl="systemctl "
+    aliasG systemctl="sudo systemctl "
+    aliasG systemctl="sudo systemctl "
+    aliasG ctl="systemctl "
+    aliasG sctl="/bin/systemctl"
     # Daemons reload
-    alias ctldr="systemctl daemon-reload"
+    aliasG ctldr="systemctl daemon-reload"
     # Credits to: https://gist.github.com/Feniksovich
-    alias ctlsp="systemctl stop "
-    alias ctlst="systemctl start "
-    alias ctlrt="systemctl restart "
-    alias ctlrl="systemctl reload "
-    alias ctls="systemctl status "
+    aliasG ctlsp="systemctl stop "
+    aliasG ctlst="systemctl start "
+    aliasG ctlrt="systemctl restart "
+    aliasG ctlrl="systemctl reload "
+    aliasG ctls="systemctl status "
     # Enable/Disable commands for units
-    alias ctle='systemctl enable '
-    alias ctld='systemctl disable '
+    aliasG ctle='systemctl enable '
+    aliasG ctld='systemctl disable '
     # Masking Units to disabling them
-    alias ctlmask='systemctl mask '
-    alias ctlunmask='systemctl unmask '
+    aliasG ctlmask='systemctl mask '
+    aliasG ctlunmask='systemctl unmask '
     # List failed units and reset systemd system status
-    alias ctlfailed='systemctl --failed --all '
-    alias ctlrf='systemctl reset-failed '
+    aliasG ctlfailed='systemctl --failed --all '
+    aliasG ctlrf='systemctl reset-failed '
 fi
 
 if command -v iptables &>/dev/null; then
-    alias it='iptables '
+    aliasG it='iptables '
     # Lookup iptables chain
-    alias itlookup="iptables --line-numbers -nvL "
+    aliasG itlookup="iptables --line-numbers -nvL "
 fi
 
 if command -v journalctl &>/dev/null; then
-    alias j="journalctl"
-    alias jf="journalctl --follow"
-    alias ju="journalctl --catalog --pager-end --unit"
-    alias juf="journalctl --follow --catalog --unit"
-    alias jfk="journalctl --follow --dmesg"
+    aliasG j="journalctl"
+    aliasG jf="journalctl --follow"
+    aliasG ju="journalctl --catalog --pager-end --unit"
+    aliasG juf="journalctl --follow --catalog --unit"
+    aliasG jfk="journalctl --follow --dmesg"
 fi
 
 if command -v apt &>/dev/null; then
-    alias apt='sudo apt '
-    alias apts='sudo apt search '
-    alias aptu='sudo apt update '
-    alias aptup='sudo apt upgrade -y '
-    alias aptdup='sudo apt dist-upgrade -y '
-    alias apti='sudo apt install -y '
-    alias aptr='sudo apt remove '
-    alias aptar='sudo apt autoremove '
-    alias aptap='sudo apt autopurge '
+    aliasG apt='sudo apt '
+    aliasG apts='sudo apt search '
+    aliasG aptu='sudo apt update '
+    aliasG aptup='sudo apt upgrade -y '
+    aliasG aptdup='sudo apt dist-upgrade -y '
+    aliasG apti='sudo apt install -y '
+    aliasG aptr='sudo apt remove '
+    aliasG aptar='sudo apt autoremove '
+    aliasG aptap='sudo apt autopurge '
 fi
 
 if command -v dnf &>/dev/null; then
-    alias dnf='sudo dnf '
-    alias dnfs='sudo dnf search '
-    alias dnfp='sudo dnf provides '
-    alias dnff='sudo dnf info '
-    alias dnfl='sudo dnf list '
-    alias dnfli='sudo dnf list --installed '
-    alias dnfq='sudo dnf repoquery '
-    alias dnfcu='sudo dnf check-update '
-    alias dnfup='sudo dnf upgrade -y '
-    alias dnfds='sudo dnf distro-sync -y '
-    alias dnfi='sudo dnf install -y '
-    alias dnfr='sudo dnf remove '
-    alias dnfar='sudo dnf autoremove '
-    alias dnfk='sudo dnf copr '
-    alias dnfuua='sudo dnfcu ; dnfup ; dnfar'
+    aliasG dnf='sudo dnf '
+    aliasG dnfs='sudo dnf search '
+    aliasG dnfp='sudo dnf provides '
+    aliasG dnff='sudo dnf info '
+    aliasG dnfl='sudo dnf list '
+    aliasG dnfli='sudo dnf list --installed '
+    aliasG dnfq='sudo dnf repoquery '
+    aliasG dnfcu='sudo dnf check-update '
+    aliasG dnfup='sudo dnf upgrade -y '
+    aliasG dnfds='sudo dnf distro-sync -y '
+    aliasG dnfi='sudo dnf install -y '
+    aliasG dnfr='sudo dnf remove '
+    aliasG dnfar='sudo dnf autoremove '
+    aliasG dnfk='sudo dnf copr '
+    aliasG dnfuua='sudo dnfcu ; dnfup ; dnfar'
 fi
 
 
 if command -v tmux &>/dev/null; then
-    alias T='tmux '
-    alias tls='tmux ls '
-    alias ta='tmux attach -t '
-    alias tn='tmux new -s '
-    alias tr='tmux rename-session -t '
-    alias tk='tmux kill-session -t '
+    aliasG T='tmux '
+    aliasG tls='tmux ls '
+    aliasG ta='tmux attach -t '
+    aliasG tn='tmux new -s '
+    aliasG tr='tmux rename-session -t '
+    aliasG tk='tmux kill-session -t '
 fi
 
 if command -v nvim &>/dev/null; then
-    alias e='nvim '
-    alias nv='nvim '
-    alias vi='nvim '
-    alias vim='nvim '
-    alias E='| nvim '
+    aliasG e='nvim '
+    aliasG nv='nvim '
+    aliasG vi='nvim '
+    aliasG vim='nvim '
+    aliasG E='| nvim '
     if command -v nvr &>/dev/null; then
-        alias nvimr='nvim --listen /tmp/nvimsocket '
-        alias nvrs='nvr -s '
+        aliasG nvimr='nvim --listen /tmp/nvimsocket '
+        aliasG nvrs='nvr -s '
     fi
 fi
 
 if command -v xclip &>/dev/null; then
-    alias Xp='xclip -o '
-    alias Xy=' | xclip -i '
+    aliasG Xp='xclip -o '
+    aliasG Xy=' | xclip -i '
 fi
 if command -v clipboard &>/dev/null; then
-    alias Cp="clipboard "
-    alias Cy=' | clipboard '
+    aliasG Cp="clipboard "
+    aliasG Cy=' | clipboard '
 fi
 if command -v wl-copy &>/dev/null; then
-    alias Wp="wl-paste "
-    alias Wy=' | wl-copy '
+    aliasG Wp="wl-paste "
+    aliasG Wy=' | wl-copy '
 fi
 
 if command -v ranger &>/dev/null; then
-    alias rw='ranger '
+    aliasG rw='ranger '
 fi
 
 if command -v fdfind &>/dev/null; then
-    alias fd='fdfind '
+    aliasG fd='fdfind '
 fi
 
 if command -v zoxide &>/dev/null; then
-    # alias cd='z '
+    # aliasG cd='z '
     echo
 fi
 
 if command -v exa &>/dev/null; then
-    alias ola='/bin/ls -lahi --color=auto '
-    alias ols='/bin/ls --color=auto '
-    alias ls="exa --icons --git --group-directories-first "
-    alias ll="exa -lg --icons --git --group-directories-first "
-    alias la="exa -aglm --icons --git --group-directories-first "
-    alias lt="exa -L 2 --icons --tree --git-ignore --group-directories-first "
+    aliasG ola='/bin/ls -lahi --color=auto '
+    aliasG ols='/bin/ls --color=auto '
+    aliasG ls="exa --icons --git --group-directories-first "
+    aliasG ll="exa -lg --icons --git --group-directories-first "
+    aliasG la="exa -aglm --icons --git --group-directories-first "
+    aliasG lt="exa -L 2 --icons --tree --git-ignore --group-directories-first "
 fi
 
 if command -v git &>/dev/null; then
-    alias g='git '
-    alias dot='dotfiles '
+    aliasG g='git '
+    aliasG dot='dotfiles '
 fi
 
 if command -v kubectl &>/dev/null; then
     source <(kubectl completion "$theShell")
-    alias k='kubectl '
+    aliasG k='kubectl '
 fi
 
 if command -v minikube &>/dev/null; then
     source <(minikube completion "$theShell")
-    alias mk='minikube '
+    aliasG mk='minikube '
 fi
 
 if command -v docker &>/dev/null; then
     source <(docker completion "$theShell")
-    alias dk='docker '
-    alias dkc='docker-compose '
+    aliasG dk='docker '
+    aliasG dkc='docker-compose '
 fi
 
 if command -v ansible &>/dev/null; then
-    alias a='ansible '
-    alias ap='ansible-playbook '
-    alias ag='ansible-galaxy '
+    aliasG a='ansible '
+    aliasG ap='ansible-playbook '
+    aliasG ag='ansible-galaxy '
 fi
 
 if command -v podman &>/dev/null; then
     source <(podman completion "$theShell")
-    alias pd='podman '
+    aliasG pd='podman '
 fi
 
 aliasG xway='env -u WAYLAND_DISPLAY '
@@ -230,11 +232,11 @@ aliasG xway='env -u WAYLAND_DISPLAY '
 if command -v curl &>/dev/null; then
     # Creditst to Jeremy "Jay" LaCroix
     # <https://www.learnlinux.tv/10-linux-terminal-tips-and-tricks-to-enhance-your-workflow/
-    alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
-    # alias myip='curl http://ipecho.net/plain; echo '
-    alias myip='curl icanhazip.com'
-    # alias wimp='curl https://wttr.in/imperia'
-    alias wimp='wth imperia'
+    aliasG speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
+    # aliasG myip='curl http://ipecho.net/plain; echo '
+    aliasG myip='curl icanhazip.com'
+    # aliasG wimp='curl https://wttr.in/imperia'
+    aliasG wimp='wth imperia'
 fi
 
 if [ "$(command -v fzf)" ] && [ "$(command -v rg)" ] && [ "$(command -v bat)" ]; then
@@ -246,7 +248,7 @@ if [ "$(command -v fzf)" ] && [ "$(command -v rg)" ] && [ "$(command -v bat)" ];
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 ;"
-    alias fzfb="fzf \
+    aliasG fzfb="fzf \
 --preview bat --border=rounded \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
@@ -275,3 +277,4 @@ else
     echo "fzf|rg|bat missing..."
 fi
 
+aliasG resetSound='/bin/systemctl --user restart pipewire.service'
