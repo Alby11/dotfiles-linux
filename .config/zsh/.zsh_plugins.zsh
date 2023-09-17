fb5d4f1a55a2b73ac6f07c32bc1fda2ee4596301
alby11 <alby11@duck.com>
2023-09-17 20:31:37 +0200

    20230917_20:31:37

--- .config/zsh/.zsh_plugins.zsh
+++ .config/zsh/.zsh_plugins.zsh
@@ -39,6 +39,8 @@ source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/lib/vcs_info.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/ag )
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/aliases )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/aliases/aliases.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/ansible )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/ansible/ansible.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/autoenv )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/autoenv/autoenv.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/battery )
@@ -62,27 +64,55 @@ fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/cp )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/cp/cp.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/debian )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/debian/debian.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/dnf )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/dnf/dnf.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/dnote )
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/docker )
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/docker-compose )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/docker-machine )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/docker-machine/docker-machine.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/emoji )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/emoji/emoji.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/emotty )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/emotty/emotty.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/extract )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/extract/extract.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/fasd )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/fasd/fasd.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/fd )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/firewalld )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/firewalld/firewalld.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/fzf )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/fzf/fzf.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/gh )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-auto-fetch )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-auto-fetch/git-auto-fetch.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-commit )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-commit/git-commit.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-extras )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/gitast )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-flow )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-lfs )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git-lfs/git-lfs.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/git/git.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/github )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/github/github.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/gitignore )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/gitignore/gitignore.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/globalias )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/globalias/globalias.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/gnu-utils )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/gnu-utils/gnu-utils.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/golang )
-source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/golang/golang.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/gpg-agent )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/gpg-agent/gpg-agent.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/history )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/history/history.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/history-substring-search )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/history-substring-search/history-substring-search.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/keychain )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/keychain/keychain.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/kubectl )
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/kubectx )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/kubectx/kubectx.plugin.zsh
@@ -90,30 +120,44 @@ fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/last-working-dir
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/last-working-dir/last-working-dir.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/magic-enter )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/magic-enter/magic-enter.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/man )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/man/man.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/minikube )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/nmap )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/nmap/nmap.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/node )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/node/node.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/npm )
-source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/npm/npm.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/nvm )
-source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/nvm/nvm.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/pep8 )
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/per-directory-history )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/per-directory-history/per-directory-history.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/pip )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/podman )
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/pyenv )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/pyenv/pyenv.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/pylint )
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/python )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/python/python.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/qrcode )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/qrcode/qrcode.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/ripgrep )
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/rsync )
-source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/rsync/rsync.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/screen )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/screen/screen.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/shrink-path )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/shrink-path/shrink-path.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/sprunge )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/sprunge/sprunge.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/ssh-agent )
-source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/ssh-agent/ssh-agent.plugin.zsh
-fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/sudo )
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/systemadmin )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/systemadmin/systemadmin.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/systemd )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/systemd/systemd.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/thefuck )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/thefuck/thefuck.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/transfer )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/transfer/transfer.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/timer )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/timer/timer.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/tmux )
@@ -122,6 +166,8 @@ fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/tmuxinator )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/tmuxinator/tmuxinator.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/ubuntu )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/ubuntu/ubuntu.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/universalarchive )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/universalarchive/universalarchive.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/urltools )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/urltools/urltools.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/virtualenv )
@@ -134,6 +180,8 @@ fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/web-search )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/web-search/web-search.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/yum )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/yum/yum.plugin.zsh
+fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/zbell )
+source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/zbell/zbell.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/zoxide )
 source /home/tallonea/.cache/antidote/ohmyzsh/ohmyzsh/plugins/zoxide/zoxide.plugin.zsh
 fpath+=( /home/tallonea/.cache/antidote/zdharma-continuum/fast-syntax-highlighting )
