1b68ffa4457e9c0fb30dd3c2c3696022c95eca1b
alby11 <alby11@duck.com>
2024-03-27 18:19:02 +0100

    20240327_18:18:44

--- .config/zsh/xxh/xxh-plugin-zsh-myconfig/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# build.sh
-
-# Set the ZDOTDIR environment variable
-echo 'export ZDOTDIR=$HOME/.xxh/.config/zsh' > $PXXH_HOME/.zshenv
-
-# Copy the zsh configuration files to the plugin directory
-mkdir -p $PXXH_HOME/.xxh/.config/zsh
-cp -r ~/.config/zsh/* $PXXH_HOME/.xxh/.config/zsh/
