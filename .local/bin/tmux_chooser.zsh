5f53400186adcb47abc67e777bf0382f69cd7e94
alby11 <alby11@duck.com>
2024-10-29 17:26:52 +0100

    fix nil exit and code refactor

--- .local/bin/tmux_chooser.zsh
+++ .local/bin/tmux_chooser.zsh
@@ -1,34 +1,38 @@
-#!/usr/bin/zsh
+#!/usr/bin/env zsh
 # shellcheck disable=SC2207
 
 # Doesn't let you press Ctrl-C
 function ctrl_c() {
-	echo -e "\renter nil to drop to normal prompt"
+    echo -e "\renter nil to drop to normal prompt"
+    read -r input
+    if [[ $input == 'nil' ]]; then
+        exit 1
+    fi
 }
 
 trap ctrl_c SIGINT
 
-no_of_terminals=$(tmux list-sessions | wc -l)
-IFS=$'\n'
-output=($(tmux list-sessions))
-output_names=($(tmux list-sessions -F\#S))
-k=1
+# Get the list of tmux sessions
+output=($(tmux list-sessions -F '#S'))
+no_of_terminals=${#output[@]}
+
 echo "Choose the terminal to attach: "
-for i in "${output[@]}"; do
-	echo "$k - $i"
-	((k++))
+for i in {1..$no_of_terminals}; do
+    echo "$i - ${output[i-1]}"
 done
+
 echo
 echo "Create a new session by entering a name for it"
 read -r input
-if [[ $input == "" ]]; then
-	tmux new-session
+
+if [[ -z $input ]]; then
+    tmux new-session
 elif [[ $input == 'nil' ]]; then
-	exit 1
-elif [[ $input =~ ^[0-9]+$ ]] && [[ $input -le $no_of_terminals ]]; then
-	terminal_name="${output_names[input - 1]}"
-	tmux attach -t "$terminal_name"
+    exit 1
+elif [[ $input =~ ^[0-9]+$ ]] && (( input > 0 && input <= no_of_terminals )); then
+    tmux attach -t "${output[input - 1]}"
 else
-	tmux new-session -s "$input"
+    tmux new-session -s "$input"
 fi
+
 exit 0
