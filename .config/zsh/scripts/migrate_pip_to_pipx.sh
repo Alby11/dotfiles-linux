6cc73fe727ccc7663a3b86b865ffe0fb94da5709
alby11 <alby11@duck.com>
2024-12-05 16:47:52 +0100

    add shell_gpt settings

--- .config/zsh/scripts/migrate_pip_to_pipx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# List of pip-installed tools to migrate
-packages=$(pip list --format=freeze | cut -d= -f1)
-
-for package in $packages; do
-  echo "Installing $package via pipx..."
-  pipx install "$package" && pip uninstall -y "$package"
-done
